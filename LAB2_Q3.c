37839078136c21fae7213f122427fb2a60bee00d
Shaminaj12 <91215423+Shaminaj12@users.noreply.github.com>
2021-09-24 00:33:45 +0600

    LAB2_Q3

---
+++ LAB2_Q3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+  int year;
+  printf("Please enter a year: ");
+  scanf("%d", &year);
+  if((year % 4 == 0 && year % 100 !=0)||(year % 400 == 0)){
+    printf("Leap Year");
+  }
+  else{
+    printf("Not a Leap year");
+  }
+  return 0;
+}
