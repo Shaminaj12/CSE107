da62da6182fe72a67122df1b7f242e3eaadc73f1
Shaminaj12 <91215423+Shaminaj12@users.noreply.github.com>
2021-09-23 15:58:10 +0600

    Add files via upload

---
+++ LAB1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int add, sub, div, mul, mod, a, b;
+    printf("Enter the first number: ");
+    scanf("%d",&a);
+    printf("Enter the first number: ");
+    scanf("%d",&b);
+    add=a+b;
+    sub=a-b;
+    div=a/b;
+    mul=a*b;
+    mod=a%b;
+    printf("The addition of two number is %d\n",add);
+    printf("The subtraction of two number is %d\n",sub);
+    printf("The division of two number is %d\n",div);
+    printf("The multiplication of two number is %d\n",mul);
+    printf("The modulu of two number is %d\n",mod);
+    return 0;
+}
+
+
