5a648f52753e90a55c50ccde7def9079460267db
Shaminaj12 <91215423+Shaminaj12@users.noreply.github.com>
2021-09-24 11:44:40 +0600

    LAB2_Q.4

--- LAB2_Q4.c
+++ LAB2_Q4.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
-int main(void){
-  int i = 1;
-  int k = 1;
-  for(i=1;i<=5;i=i+1){
-    k = k*i;
-  }
-  printf("%d\n",k);
+
+int main(void) {
+  int a,b,c;
+  printf("Please enter a number:");
+  scanf("%d", &a);
+  printf("Please enter a number:");
+  scanf("%d", &b);
+  printf("Please enter a number: ");
+  scanf("%d", &c);
+  if(a > b && a>c){
+    printf("largest number: %d\n", a);
+  }else if(b>a && b>c){
+    printf("largest number: %d\n", b);
+  }else if(c>a && c>b){
+    printf("largest number: %d\n", c);
   
-  return 0;
+  }
+  if(a<b && a<c){
+    printf("smallest number: %d\n", a);
+  }else if(b<a && b<c){
+    printf("smallest number: %d\n", b);
+  }else if(c<a && c<b){
+    printf("smallest number: %d\n", c);
+  }
+   return 0;
 }
